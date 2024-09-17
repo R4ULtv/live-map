c9995fe27118c940f6b97ca0d024392ad3edd18b
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-17 15:29:14 +0200

    fix fill color

--- components/WorldMap.js
+++ components/WorldMap.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { scaleLinear } from "d3-scale";
 import {
   ComposableMap,
   Geographies,
@@ -7,19 +8,29 @@ import {
   ZoomableGroup,
 } from "react-simple-maps";
 
-export default function WorldMap({ geoData }) {
-    console.log(geoData);
+export default function WorldMap({ geoData, min, max }) {
+  const colorScale = scaleLinear()
+    .domain([min, max])
+    .range(["#a1a1aa", "#fafafa"]);
+
   return (
     <ComposableMap style={{ width: "100%", height: "100vh" }}>
-      <ZoomableGroup zoom={1.25} minZoom={1} maxZoom={5} center={["10", "0"]}>
-        <Geographies geography="countries-110m.json">
+      <ZoomableGroup
+        zoom={1.25}
+        minZoom={1.25}
+        maxZoom={5}
+        center={["10", "0"]}
+      >
+        <Geographies geography="countries.json">
           {({ geographies }) =>
             geographies.map((geo) => {
+              const d = geoData.find((s) => s._id === geo.properties.Alpha2);
               return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
-                  className="stroke-zinc-800 stroke-0.5 fill-zinc-500 outline-none"
+                  className="stroke-zinc-800 stroke-0.5 outline-none"
+                  fill={d ? colorScale(d["count"]) : "#71717a"}
                 />
               );
             })
