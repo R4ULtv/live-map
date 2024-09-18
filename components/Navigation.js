87a1db404cd6f0fa42e5585c04cfc4224622f67b
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-18 11:01:35 +0200

    sync with cmdk

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,35 +1,42 @@
 "use client";
 
-import { MagnifyingGlassIcon, UserIcon } from "@heroicons/react/16/solid";
-import { useState } from "react";
-import RequestDialog from "./RequestDialog";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "@heroicons/react/20/solid";
 import { Button } from "@headlessui/react";
+import { useCommandMenu } from "@/components/CommandMenuContext";
 
 export default function Navigation({ requestInfo }) {
-  const [requestOpen, setRequestOpen] = useState(false);
+  const { setIsOpen, setIsCountrySearchOpen, setIsRequestInfoOpen } = useCommandMenu();
 
   return (
     <>
-      <div className="absolute bottom-2 right-0 left-0 flex items-center justify-center">
+      <div className="absolute bottom-5 right-0 left-0 flex items-center justify-center">
         <div className="rounded-full border border-zinc-700 bg-zinc-900 text-zinc-200 shadow flex items-center">
           <Button
-            onClick={() => setRequestOpen(true)}
-            className="p-2 data-[hover]:bg-zinc-800 data-[focus]:bg-zinc-800 first:rounded-l-full last:rounded-r-full outline-none"
+            onClick={() => setIsRequestInfoOpen(true)}
+            className="p-2 data-[hover]:bg-zinc-800 data-[focus]:bg-zinc-800 first:rounded-l-full last:rounded-r-full outline-none relative group"
           >
             <UserIcon className="size-5" />
           </Button>
 
-          <Button className="p-2 data-[hover]:bg-zinc-800 data-[focus]:bg-zinc-800 first:rounded-l-full last:rounded-r-full outline-none">
+          <Button
+            onClick={() => setIsCountrySearchOpen(true)}
+            className="p-2 data-[hover]:bg-zinc-800 data-[focus]:bg-zinc-800 first:rounded-l-full last:rounded-r-full outline-none"
+          >
             <MagnifyingGlassIcon className="size-5" />
+          </Button>
+
+          <Button
+            onClick={() => setIsOpen(true)}
+            className="p-2 data-[hover]:bg-zinc-800 data-[focus]:bg-zinc-800 first:rounded-l-full last:rounded-r-full outline-none"
+          >
+            <Bars3Icon className="size-5" />
           </Button>
         </div>
       </div>
-
-      <RequestDialog
-        requestInfo={requestInfo}
-        isOpen={requestOpen}
-        setIsOpen={setRequestOpen}
-      />
     </>
   );
 }
