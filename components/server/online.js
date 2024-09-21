86eb895a8cf12fe253ff374fa79dff2674bd483d
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-21 12:16:35 +0200

    removed

--- components/server/online.js
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { kv } from "@vercel/kv";
-
-export async function getOnlineCount() {
-  const onlineUsers = await kv.keys("@live-map:online:*");
-  return onlineUsers.length;
-}
