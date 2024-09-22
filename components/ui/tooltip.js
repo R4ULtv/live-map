4bafebe98c097f905a31792a9b99b825d3dcd858
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-22 18:57:25 +0200

    small fix

--- components/ui/tooltip.js
+++ components/ui/tooltip.js
@@ -16,7 +16,7 @@ const TooltipContent = forwardRef((props, ref) => {
       ref={ref}
       sideOffset={sideOffset}
       className={
-        "z-50 overflow-hidden rounded-xl border bg-zinc-900 px-3 py-1.5 text-sm mb-0.5 text-zinc-200 border-zinc-800 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2" +
+        "z-50 overflow-hidden rounded-xl border bg-zinc-900 px-3 py-1.5 text-sm m-0.5 text-zinc-200 border-zinc-800 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2" +
         className
       }
       {...rest}
