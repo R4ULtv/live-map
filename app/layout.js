9f8a7ea0920aaeb46e39c2ba3d442f42c3542246
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-26 11:06:25 +0200

    add theme provider

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,27 @@
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { CommandMenuProvider } from "@/components/providers/CommandMenuContext";
+import { ThemeProvider } from "next-themes";
 
 export const metadata = {
   title: "Live Map - Simple World Map",
-  description: "A world map with stats and general information for each country.",
+  description:
+    "A world map with stats and general information for each country.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="antialiased scroll-smooth">
+    <html
+      lang="en"
+      className="antialiased scroll-smooth"
+      suppressHydrationWarning
+    >
       <body
         className={`${GeistSans.className} bg-zinc-100 dark:bg-zinc-900 selection:bg-zinc-400/25 dark:selection:bg-zinc-600/25 relative`}
       >
-        <CommandMenuProvider>
-          {children}
-        </CommandMenuProvider>
+        <ThemeProvider attribute="class" enableSystem={false}>
+          <CommandMenuProvider>{children}</CommandMenuProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
